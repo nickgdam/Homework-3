e3ab358d2188adb49e1634ba33e0c76fa45c9a48
nickgdam <68656660+nickgdam@users.noreply.github.com>
2020-09-07 15:22:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+
+//Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var allChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// Variable Declaration 
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+// Prompt to confirm how many characters the user would like in their password
+function generatePassword() {
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
+
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-128 characters. Please try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+    // Determine parameters of password 
+    var confirmUpperCase = prompt("What type of characters would you like to use? (Please type 'Uppercase', 'Lowercase', 'Numeric', Special' or 'All'.)");
+      // Loop if answer is outside the parameters 
+      
+      // Assign an action to the password parameters NEED TO FIX THIS
+      var passwordCharacters = []
+
+     
+      
+      
+    if (confirmUpperCase === "special") {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    }
+
+    if (confirmUpperCase === "numeric") {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+
+    if (confirmUpperCase === "lowercase") {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (confirmUpperCase === "uppercase") {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
+
+    if (confirmUpperCase === "all"){
+      passwordCharacters = passwordCharacters.concat(allChar)
+
+    }
+
+    
+
+      console.log(passwordCharacters)
+
+      // Empty string to be filled based on for loop selecting random characters from the array
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
